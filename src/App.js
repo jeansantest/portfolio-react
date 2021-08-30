f2893c4ac1bb79b8328c26fa7e30a1d4f70c248d
Jean Esteves <jean.esteves2001@gmail.com>
2021-08-30 13:13:11 -0300

    voltando as origens

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import { Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from './Context/ThemeContext';
 import './App.css';
 import Home from './Pages/Home';
-import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <ThemeProvider>
       <Switch>
-        <Route path="/" component={Navbar} />
         <Route exact path="/" component={Home} />
       </Switch>
     </ThemeProvider>
